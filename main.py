9acad7f582c92751d9d634d68e13bac9b69fb14b
Github-sanket07sett <137095374+Github-sanket07sett@users.noreply.github.com>
2023-12-16 18:07:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,3 +15,42 @@ figure.show()
 #highest scorer in total matches
 figure=px.bar(data,x=data["highest scorer"],y=data["runs"],color="highest scorer")
 figure.show()
+#TOSS DECISIONS
+toss = data["Toss decision"].value_counts()
+label = toss.index
+counts = toss.values
+colors = ['red','lightgreen']
+
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='toss desicions in world cup 2023')
+fig.update_traces(hoverinfo='percent', textinfo='value', textfont_size=30,
+                  marker=dict(colors=colors, line=dict(color='black', width=3)))
+fig.show()
+# WON BY RUNS OR WICKETS
+won_by = data["won by"].value_counts()
+label = won_by.index
+counts = won_by.values
+colors = ['red','lightgreen']
+
+fig = go.Figure(data=[go.Pie(labels=label, values=counts)])
+fig.update_layout(title_text='Number of Matches Won By Runs/Wickets')
+fig.update_traces(hoverinfo='percent', textinfo='value', textfont_size=30,
+                  marker=dict(colors=colors, line=dict(color='black', width=3)))
+fig.show()
+#best stadium to bowl first
+fig = go.Figure()
+fig.add_trace(go.Bar(x=data["Venue"],y=data["1st innings wickets"],name='First innings wickets',marker_color='blue'))
+fig.add_trace(go.Bar(x=data["Venue"],y=data["2nd innings wickets"],name='Second innings wickets',marker_color='green'))
+fig.update_layout(barmode='group',xaxis_tickangle=-15,title=" best stadium to bowl first")
+fig.show()
+#best stadium to bat first
+fig = go.Figure()
+fig.add_trace(go.Bar(x=data["Venue"],y=data["1st innings score"],name='First innings score',marker_color='blue'))
+fig.add_trace(go.Bar(x=data["Venue"],y=data["2nd innings score"],name='Second innings score',marker_color='red'))
+fig.update_layout(barmode='group',xaxis_tickangle=-15,title=" best stadium to bat first")
+fig.show()
+#best scores and wicket taker in each time
+figure=px.bar(data,x=data["highest scorer"],y=data["runs"],color="highest scorer",title="highest run scorer in each match")
+figure.show()
+figure=px.bar(data,x=data["highest wickets"],title=" best bowler in world cup 2023")
+figure.show()
